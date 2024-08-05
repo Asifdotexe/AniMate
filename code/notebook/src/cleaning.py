a74bb55de78fbc978ad38a0c7086b70f1ecaa8de
Pranjal Dhamane <pranjaldhamane37@gmail.com>
2024-08-05 07:14:05 +0530

    #7 added punctuation removal functionality

--- code/notebook/src/cleaning.py
+++ code/notebook/src/cleaning.py
@@ -1,4 +1,8 @@
 import re
+import string
+
+import re
+import string
 
 def nlp_cleaning(text):
     """
@@ -9,18 +13,22 @@ def nlp_cleaning(text):
 
     Returns:
     str: The cleaned text after applying the following operations:
-         - Convert the text to lowercase.
-         - Remove URLs from the text.
-         - Remove non-ASCII characters.
-         - Remove numbers.
-         - Remove extra whitespaces.
+         
+    - Convert the text to lowercase.
+    - Remove URLs from the text.
+    - Remove non-ASCII characters.
+    - Remove numbers.
+    - Remove punctuation.
+    - Remove extra whitespaces.
     """
     if not isinstance(text, str):
         return ''  # Handle non-text values
 
     text = text.lower()
-    text = re.sub(r'http[s]?://\S+', '', text)
-    text = text.encode('ascii', 'ignore').decode('ascii')
-    text = re.sub(r'\d+', '', text)
-    text = re.sub(r'\s+', ' ', text).strip()
+    text = re.sub(r'http[s]?://\S+', '', text)  # Remove URLs
+    text = text.encode('ascii', 'ignore').decode('ascii')  # Remove non-ASCII characters
+    text = re.sub(r'\d+', '', text)  # Remove numbers
+    text = text.translate(str.maketrans('', '', string.punctuation))  # Remove punctuation
+    text = re.sub(r'\s+', ' ', text).strip()  # Remove extra whitespaces
+    
     return text
