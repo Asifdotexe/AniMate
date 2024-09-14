93dd1fecebd0af0bc5ffbb36c51114579fbebb06
Asif Sayyed <asifdotexe@gmail.com>
2024-09-14 13:57:13 +0530

    #3 downloading punkt-tab for solving LookUpError

--- app.py
+++ app.py
@@ -1,23 +1,17 @@
+import nltk
 import streamlit as st
 import pandas as pd
+from nltk.corpus import stopwords
 from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.neighbors import NearestNeighbors
 from nltk.tokenize import word_tokenize
 from nltk.stem import PorterStemmer
+nltk.download('stopwords')
+nltk.download('punkt_tab')
 
 stemmer = PorterStemmer()
 data = pd.read_csv('data/final/processed_data_02092024.csv')
-stop_words = set([
-    'i', 'me', 'my', 'myself', 'we', 'our', 'ours', 'ourselves', 'you', 'your', 'yours', 'yourself', 'yourselves',
-    'he', 'him', 'his', 'himself', 'she', 'her', 'hers', 'herself', 'it', 'its', 'itself', 'they', 'them', 'their',
-    'theirs', 'themselves', 'what', 'which', 'who', 'whom', 'this', 'that', 'these', 'those', 'am', 'is', 'are', 'was',
-    'were', 'be', 'been', 'being', 'have', 'has', 'had', 'having', 'do', 'does', 'did', 'doing', 'a', 'an', 'the', 'and',
-    'but', 'if', 'or', 'because', 'as', 'until', 'while', 'of', 'at', 'by', 'for', 'with', 'about', 'against', 'between',
-    'into', 'through', 'during', 'before', 'after', 'above', 'below', 'to', 'from', 'up', 'down', 'in', 'out', 'on', 'off',
-    'over', 'under', 'again', 'further', 'then', 'once', 'here', 'there', 'when', 'where', 'why', 'how', 'all', 'any', 'both',
-    'each', 'few', 'more', 'most', 'other', 'some', 'such', 'no', 'nor', 'not', 'only', 'own', 'same', 'so', 'than', 'too',
-    'very', 's', 't', 'can', 'will', 'just', 'don', 'should', 'now'
-])
+stop_words = set(stopwords.words('english'))
 
 def preprocess_text(text: str) -> str:
     """
